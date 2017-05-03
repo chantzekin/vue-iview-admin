7914c8c4ee0f075d5ebd16e8c97dec765dca9e36
naraku777 <442815900@qq.com>
2017-05-03 18:01:47 +0800

    Add Pages

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,13 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const Index = r =>
-  require.ensure([], () => r(require('@/pages/Index')), 'index')
-const Dashboard = r =>
-  require.ensure([], () => r(require('@/pages/Dashboard')), 'dashboard')
-const Login = r =>
-  require.ensure([], () => r(require('@/pages/User/Login')), 'login')
+const Index = r => require.ensure([], () => r(require('@/pages/Index')), 'index')
+const Dashboard = r => require.ensure([], () => r(require('@/pages/Dashboard')), 'dashboard')
+const Task = r => require.ensure([], () => r(require('@/pages/Task')), 'task')
+
+const NotFound = r => require.ensure([], () => r(require('@/pages/404')), '404')
+
+const Login = r => require.ensure([], () => r(require('@/pages/User/Login')), 'login')
 
 const routes = [
   {
@@ -18,11 +19,24 @@ const routes = [
     meta: { title: '主页', auth: true },
     children: [
       {
+        alias: '',
         path: '/dashboard',
         name: 'dashboard',
         component: Dashboard,
         meta: { title: '仪表盘', auth: true },
-      }
+      },
+      {
+        path: '/task',
+        name: 'task',
+        component: Task,
+        meta: { title: '案例', auth: true },
+      },
+      {
+        path: '*',
+        name: '404',
+        component: NotFound,
+        meta: { title: '404 Not Found', auth: true },
+      },
     ]
   },
   {
