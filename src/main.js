818a1e6df64071f6a89b479f8c1c7a09d82ef731
naraku777 <442815900@qq.com>
2017-05-12 10:35:32 +0800

    add nprogress

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue'
 import iView from 'iview'
 import axios from 'axios'
+import NProgress from 'nprogress'
 
 import App from './App'
 import router from './router'
 import store from './store'
 import mcok from './mock'
 
+import 'nprogress/nprogress.css'
 import 'iview/dist/styles/iview.css'
 import './assets/common.css'
 
@@ -17,6 +19,19 @@ Vue.use(iView)
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+NProgress.configure({ showSpinner: false })
+
+const notNeedAuthRoutes = ['/login']
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next()
+
+})
+router.afterEach(() => {
+  NProgress.done();
+});
+
 
 /* eslint-disable no-new */
 new Vue({
