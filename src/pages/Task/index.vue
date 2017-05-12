fe6a72e7f94572de426d1a5063ef725b47a8a201
naraku777 <442815900@qq.com>
2017-05-12 18:24:28 +0800

    init task page

---
+++ src/pages/Task/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content__inner">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'task',
+  data() {
+    return {
+    }
+  },
+  components: {
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
